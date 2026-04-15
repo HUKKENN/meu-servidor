d21a32200d2d67eb607da30088d8f62b3c223468
HUKKENN <121576548+HUKKENN@users.noreply.github.com>
2026-04-15 11:03:40 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+dados = {"valor": 0.0}
+
+@app.route("/set", methods=["POST"])
+def set_valor():
+    global dados
+    dados["valor"] = float(request.json["valor"])
+    return "OK"
+
+@app.route("/get", methods=["GET"])
+def get_valor():
+    return jsonify(dados)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
